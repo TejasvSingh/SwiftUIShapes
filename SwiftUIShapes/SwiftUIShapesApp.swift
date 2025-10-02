d9541111f7c373228002645fa6874bc12a76d07c
TejasvSingh <singhtejasv98@gmail.com>
2025-10-02 10:53:42 -0500

    Initial Commit

---
+++ SwiftUIShapes/SwiftUIShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIShapesApp.swift
+//  SwiftUIShapes
+//
+//  Created by Tejasv Singh on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
