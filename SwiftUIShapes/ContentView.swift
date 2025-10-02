3175eb49d2088065d798395be809850934d47553
TejasvSingh <singhtejasv98@gmail.com>
2025-10-02 11:16:25 -0500

    Initial Commit

--- SwiftUIShapes/ContentView.swift
+++ SwiftUIShapes/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle().fill(Color.blue)
+            Circle().stroke(lineWidth: 5)
+            Circle().stroke(Color.green, lineWidth: 5)
+            Circle().stroke(Color.orange, style: StrokeStyle(lineWidth: 25, lineCap: .butt, dash: [10]))
+            Circle().stroke(Color.purple, style: StrokeStyle(lineWidth: 25, lineCap: .round, dash: [20]))
+            Circle().trim(from: 0.25, to: 0.5).stroke(Color.orange, lineWidth: 25)
+            Ellipse().frame(width: 200, height: 100)
+            Capsule(style: .circular)
+            Rectangle().trim(from: 0.5, to: 1)
+            RoundedRectangle(cornerRadius: 50).foregroundColor(.yellow)
         }
         .padding()
     }
